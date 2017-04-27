f8887791ac99b14f745bf031af7fedfd6d1e4ea7
giorgia <giorgia@Giorgia>
2017-04-27 17:52:19 +0200

    inizio m3

---
+++ NerdBook/src/java/amm/nerdbook/classi/UtenteRegistrato.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amm.nerdbook.classi;
+
+/**
+ *
+ * @author giorgia
+ */
+public class UtenteRegistrato {
+    
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String urlProfilo;
+    private String datanascita;
+    private String frase;
+    private String password;
+    
+    
+     public UtenteRegistrato() {
+        id = 0;
+        nome = "";
+        cognome = "";
+        email = "";
+        password = "";
+        frase="";
+        datanascita="";
+        urlProfilo="";
+     }
+     
+     
+     
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getCognome() {
+        return cognome;
+    }
+
+    /**
+     * @param cognome il cognome
+     */
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+   
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+     /**
+     * @return la data di nascita
+     */
+    public String getDataNascita() {
+        return password;
+    }
+
+    /**
+     * @param datanascita data di nascuta
+     */
+    public void setDataNascita(String datanascita) {
+        this.datanascita = datanascita;
+    }
+    
+     /**
+     * @return la frase di presetazione
+     */
+    public String getFrase() {
+        return password;
+    }
+
+    /**
+     * @param frase frase di presentazione
+     */
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+    
+    /**
+     * @return the urlProfilo
+     */
+    public String getUrlProfilo() {
+        return urlProfilo;
+    }
+    
+    /**
+     * @param urlProfilo url del profilo
+     */
+    public void setUrlProfilo(String urlProfilo){
+        this.urlProfilo = urlProfilo;
+    }
+
+    @Override
+    public boolean equals(Object altroUtente) {
+        if (altroUtente instanceof UtenteRegistrato)
+            if (this.getId() == ((UtenteRegistrato)altroUtente).getId()) return true;
+        return false;
+    }
+
+    
+
+    
+}
---
+++ NerdBook/src/java/amm/nerdbook/classi/UtenteRegistratoFactory.java
@@ -0,0 +1,84 @@
+
+package amm.nerdbook.classi;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author giorgia
+ */
+public class UtenteRegistratoFactory {
+   
+    private static UtenteRegistratoFactory singleton;
+
+    public static UtenteRegistratoFactory getInstance() {
+        if (singleton == null) {
+            singleton = new UtenteRegistratoFactory();
+        }
+        return singleton;
+    }
+
+    private ArrayList<UtenteRegistrato> listaUtenti = new ArrayList<UtenteRegistrato>();
+
+    private UtenteRegistratoFactory() {
+        //Creazione utenti
+
+        //Bob Dog
+        UtenteRegistrato utente1 = new UtenteRegistrato();
+        utente1.setId(0);
+        utente1.setNome("Bob");
+        utente1.setCognome("Dog");
+        utente1.setDataNascita("26 Giugno 1993");
+        utente1.setFrase("Amo corrrere nei prati");
+        utente1.setEmail("bobdog@hotmail.it");
+        utente1.setPassword("254866");
+        utente1.setUrlProfilo("image/dog.PNG");
+
+        //Lisa Simpson
+        UtenteRegistrato utente2 = new UtenteRegistrato();
+        utente2.setId(1);
+        utente2.setNome("Lisa");
+        utente2.setCognome("Simpson");
+        utente2.setDataNascita("14 Genaio 1990");
+        utente2.setFrase("Il giallo è il mio colore preferito");
+        utente2.setEmail("simpson.lisa@hotmail.it");
+        utente2.setPassword("982547456");
+        utente2.setUrlProfilo("image/profiloLisa.PNG");
+        
+        
+        //Dexter
+        
+        UtenteRegistrato utente3 = new UtenteRegistrato();
+        utente3.setId(2);
+        utente3.setNome("Dexter");
+        utente3.setDataNascita("31 Dicembre 1995");
+        utente3.setFrase("Il mio laboratorio è unico");
+        utente3.setEmail("dexter@hotmail.it");
+        utente3.setPassword("58464dc4x5");
+        utente3.setUrlProfilo("image/Dexter.jpg");
+        
+        listaUtenti.add(utente1);
+        listaUtenti.add(utente2);
+        listaUtenti.add(utente3);
+        
+       
+    }
+
+    public UtenteRegistrato getUtentiId(int id) {
+        for (UtenteRegistrato utente : this.listaUtenti) {
+            if (utente.getId() == id) {
+                return utente;
+            }
+        }
+        return null;
+    }
+    
+     public int getIdByUserAndPassword(String user, String password){
+        for(UtenteRegistrato utente : this.listaUtenti){
+            if(utente.getNome().equals(user) && utente.getPassword().equals(password)){
+                return utente.getId();
+            }
+        }
+        return -1;
+    }
+   
+}
